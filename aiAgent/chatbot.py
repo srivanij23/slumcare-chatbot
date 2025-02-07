f2fe6b84e09e175c6506c2ed4912186ed3287fab
srivanij23 <add.>
2025-02-07 17:03:46 +0530

    Updated chatbot.py with new changes

--- aiAgent/chatbot.py
+++ aiAgent/chatbot.py
@@ -5,11 +5,11 @@ import streamlit as st
 
 class AIAgent:
     def __init__(self,
-                 category_model_path='aiAgent/category_model.pkl',
-                 authority_model_path='aiAgent/authority_model.pkl',
-                 category_encoder_path='aiAgent/category_encoder.pkl',
-                 authority_encoder_path='aiAgent/authority_encoder.pkl',
-                 vectorizer_path='aiAgent/vectorizer.pkl'):
+                 category_model_path='category_model.pkl',
+                 authority_model_path='authority_model.pkl',
+                 category_encoder_path='category_encoder.pkl',
+                 authority_encoder_path='authority_encoder.pkl',
+                 vectorizer_path='vectorizer.pkl'):
 
         try:
             self.category_model = joblib.load(category_model_path)
